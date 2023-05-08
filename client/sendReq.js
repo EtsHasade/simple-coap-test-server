b80c5fa382984d6dfec8584b20894f11042f28e9
Gilad <gilad@auraair.io>
2023-05-08 12:27:07 +0300

    first commit

---
+++ client/sendReq.js
@@ -0,0 +1,12 @@
+const coap = require('coap')
+
+process.stdin.on('data', data => {
+    const [method, msg] = data.toString().split(':')
+    const req = coap.request(`coap://192.168.1.4/${method}/${msg}`)
+
+    req.on('response', (res) => {
+        res.pipe(process.stdout)
+    })
+    req.end()
+})
+
---
+++ server.js
@@ -0,0 +1,43 @@
+require('./client/sendReq.js')
+///////////
+
+const coap = require('coap')
+
+const server = coap.createServer()
+
+server.on('request', (req, res) => {
+    const [domain, method, msg] = req.url.split('/')
+    if (method === 'quit') process.exit(0)
+
+    switch (method) {
+        case 'GET':
+            onGet(req, res, msg)
+            break;
+        case 'post':
+            onPost(req, res, msg)
+            break;
+
+        default:
+            break;
+    }
+
+
+})
+
+function onGet(req, res, msg) {
+    res.end('GET >> ' + msg + '\n')
+}
+
+
+function onPost(req, res, msg) {
+    res.end('POST >> ' + msg + '\n')
+}
+
+
+// the default CoAP port is 5683
+const port = 5683;
+server.listen(port, () => {
+    console.log('coap server listen on port coap://localhost/' + port);
+    console.log('send coap like that: "GET:msg"');
+})
+
